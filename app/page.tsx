e5eddbcf3912ed79726a3f203abc5e89b97c0b00
bisre1921 <bisrattewodros3@gmail.com>
2025-03-19 10:25:24 +0300

    add tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import AuthForm from "./pages/AuthPage"; 
+import AuthPage from "./pages/AuthPage"; 
+import "./globals.css";
 
 export default function Home() {
   return (
     <div>
-      <h1>Agazh</h1>
-      <AuthForm />
+      <h1 className="text-3xl font-bold underline">Agazh</h1>
+      <AuthPage />
     </div>
   );
 }
